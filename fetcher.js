3327158cd4ee60addebd9d89916edf9268f35556
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-10-25 18:15:04 -0700

    adding fetcher

---
+++ fetcher.js
@@ -0,0 +1,33 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: node fetcher.js <URL> <file-path>');
+  process.exit(1);
+}
+
+https.get(url, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  response.on('end', () => {
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+        process.exit(1);
+      }
+
+      const fileSize = Buffer.byteLength(data);
+      console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`);
+    });
+  });
+}).on('error', (err) => {
+  console.error('Error downloading the file:', err);
+  process.exit(1);
+});
